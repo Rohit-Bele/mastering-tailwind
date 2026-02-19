10794eef4963889c3aca5db51b87c0eac0458a98
Rohit-Bele <rohit.bele3718@gmail.com>
2026-02-19 11:04:41 +0530

    feat: initial Tailwind setup and first centered card UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+
+function App() {
+
+  return (
+    <div className="w-full h-screen m-0 p-0 flex justify-center items-center bg-black ">
+    <div className='text-blue-500 text-center  font-extrabold text-[30px] border-x border-y pt-[100px] pb-[100px] pl-[50px] pr-[50px] rounded-3xl'>
+     <h1> hello world</h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
